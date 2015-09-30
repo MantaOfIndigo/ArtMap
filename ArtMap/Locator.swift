6e5f65ca3089e9cb4c3d0c2cf5fccf2597d32dde
Andrea Mantani <offtheheezay@hotamil.it>
2015-09-30 19:15:15 +0200

    Update Locator.swift

--- ArtMap/Locator.swift
+++ ArtMap/Locator.swift
@@ -9,35 +9,71 @@
 import UIKit
 import CoreLocation
 
+enum LocatorErrors: Int{
+    case AuthorizationDenied
+    case AuthorizationNotDetermined
+    case InvalidLocation
+}
+
 class Locator: NSObject, CLLocationManagerDelegate {
     // ATTRIBUTO LOCATOR ---------------------------------------------
-    var locationManager:CLLocationManager = CLLocationManager()
-    // COSTRUTTORI ---------------------------------------------------
-    override init(){
-        super.init()
-        self.locationManager.delegate = self
-        self.locationManager.requestAlwaysAuthorization()
-        
+    var locationManager:CLLocationManager?
+    
+    //DISTRUTTORE ----------------------------------------------------
+    deinit{
+        locationManager?.delegate = nil
+        locationManager = nil
+    }
+    
+    typealias LocationClosure = ((location: CLLocation?, error: NSError?)->())
+    private var didComplete: LocationClosure?
+    
+    private func _didComplete(location: CLLocation?, error: NSError?){
+        locationManager?.stopUpdatingLocation()
+        didComplete?(location: location, error: error)
+        locationManager?.delegate = nil
+        locationManager = nil
     }
     
     func locationManager(manger: CLLocationManager!, didChangeAuthoricationStatus status: CLAuthorizationStatus){
         
-        print("didChangeAuthoricationStatus")
-        
         switch status{
-        case .NotDetermined:
-            print(".NotDetermined")
-            break
-            
-        case .Authorized:
-            print(".Authorized")
-            break
+        case .AuthorizedWhenInUse:
+            self.locationManager!.startUpdatingLocation()
         case .Denied:
-            print(".Denied")
-            break
+            _didComplete(nil, error: NSError(domain: self.classForCoder.description(), code: LocatorErrors.AuthorizationDenied.rawValue, userInfo: nil))
         default:
-            print("Unhandled authorization status")
             break
+        }
+    }
+    
+    internal func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        _didComplete(nil, error: error)
+    }
+    internal func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations[0] as? CLLocation{
+            _didComplete(location, error: nil)
+        } else {
+            _didComplete(nil, error: NSError(domain: self.classForCoder.description(),
+                code: LocatorErrors.InvalidLocation.rawValue,
+                userInfo: nil))
+
+        }
+        
+    }
+    
+    func fetchWithCompletion(completion: LocationClosure){
+        didComplete = completion
+        
+        locationManager = CLLocationManager()
+        locationManager!.delegate = self
+        
+        if(NSBundle.mainBundle().objectForInfoDictionaryKey("NSLocationWhenInUseUsageDescription") != nil){
+            locationManager!.requestWhenInUseAuthorization()
+        } else if(NSBundle.mainBundle().objectForInfoDictionaryKey("NSLocationAlwaysUsageDescription") != nil){
+            locationManager!.requestAlwaysAuthorization()
+        } else {
+            fatalError("Per usare la locazione in iOS8 occorre defiire  NSLocationWhenInUseUsageDescription o NSLocationAlwaysUsageDescription nei bundle dell'app nel file Info.plist")
         }
     }
     
