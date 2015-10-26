df02af98a764e3f332420dfb7e9e70da9945f475
Andrea Mantani <andreamantani@iMac-di-Andrea.local>
2015-10-26 18:08:18 +0100

    26-ott

--- ArtMap/ReportViewController.swift
+++ ArtMap/ReportViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 
-class ReportViewController : UIViewController{
+class ReportViewController : UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
 
+    @IBOutlet weak var setVisibility: UIPickerView!
+    
+    var pickerValues: [String] = [String]()
+    
+    @IBAction func back(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func sendReport(sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setVisibility.dataSource = self
+        setVisibility.delegate = self
+        pickerValues = ["Visibile","Rovinato o Inaccessibile","Rimosso o Distrutto"]
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerValues[row]
+    }
 }
